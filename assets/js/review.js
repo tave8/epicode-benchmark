19336c9349a4b49a2b192217cf3f8acdf2cbe866
Ernesto <ernesto.scorzelli77@gmail.com>
2025-12-17 17:03:01 +0100

    finale giovedi

--- assets/js/review.js
+++ assets/js/review.js
@@ -26,7 +26,17 @@ const stelle = document.querySelectorAll(".stella");
 const stellaAccesa = "./assets/img/star.svg";
 const stellaSpenta = "./assets/img/star-blue.svg";
 
+function updateStars(value) {
+  stelle.forEach((star) => {
+    star.classList.toggle(stellaAccesa, star.dataset.value <= value);
+  });
+}
+
 stelle.forEach((stellaAttuale, indiceCliccato) => {
+  stelle.forEach((star) => {
+    updateStars(star.dataset.value);
+  });
+
   stellaAttuale.addEventListener("click", () => {
     stelle.forEach((stellaDaAggiornare, indiceAttuale) => {
       //se l'indice cliccato minore o uguale le stelle restano spente
